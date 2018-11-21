a4c69da81675b3550d5156d5854ddec54d84d419
Wataru Inoue <southavem@gmail.com>
2018-11-21 16:29:15 +0900

    Update bhnet.py

--- python/black_hat/chap2/bhnet.py
+++ python/black_hat/chap2/bhnet.py
@@ -36,4 +36,53 @@ def usage():
     sys.exit(0)
 
 
-usage()
+def main():
+    global listen
+    global port
+    global execute
+    global command
+    global upload_destination
+    global target
+
+    if not len(sys.argv[1:]):
+        usage()
+
+    try:
+        opts, args = getopt.getopt(
+            sys.argv[1:],
+            "hle:t:p:cu:",
+            ["help", "listen", "execute=", "target=",
+             "port=", "command", "upload="])
+
+    except getopt.GetoptError as err:
+        print(str(err))
+        usage()
+
+    for o, a in opts:
+        if o in ("-h", "--help"):
+            usage()
+        elif o in ("-l", "--listen"):
+            listen = True
+        elif o in ("-e", "--execute"):
+            execute = a
+        elif o in ("-c", "--commandshell"):
+            command = True
+        elif o in ("-u", "--upload"):
+            upload_destination = a
+        elif o in ("-t", "--target"):
+            target = a
+        elif o in ("-p", "--port"):
+            port = int(a)
+        else:
+            assert False, "Unhandled Option"
+
+    if not listen and len(target) and port > 0:
+
+        buffer = sys.stdin.read()
+
+        client_sender(buffer)
+
+    if listen:
+        server_loop()
+
+main()
