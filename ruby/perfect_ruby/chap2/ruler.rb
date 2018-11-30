6556f1828cdc0506791ff33411743010b1ace3c2
Wataru Inoue <southavem@gmail.com>
2018-11-30 18:00:10 +0900

    Update ruler.rb

--- ruby/perfect_ruby/chap2/ruler.rb
+++ ruby/perfect_ruby/chap2/ruler.rb
@@ -17,3 +17,29 @@ puts ruler.length
 class Ruler
   attr_accessor :length
 end
+
+
+class Ruler
+  attr_accessor :length
+
+  def set_default_length
+    self.length = 30
+  end
+end
+
+ruler = Ruler.new
+ruler.set_default_length
+
+puts ruler.length
+
+
+class Ruler
+  attr_accessor :length
+
+  def initialize(length)
+    @length = length
+  end
+end
+
+ruler = Ruler.new(30)
+puts ruler.length
