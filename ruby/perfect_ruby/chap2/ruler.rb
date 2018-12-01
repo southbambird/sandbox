8403d41ad740910d7318746eea41fabecc49355d
Wataru Inoue <southavem@gmail.com>
2018-12-01 17:09:21 +0900

    Update ruler.rb

--- ruby/perfect_ruby/chap2/ruler.rb
+++ ruby/perfect_ruby/chap2/ruler.rb
@@ -55,3 +55,21 @@ end
 
 
 puts Ruler.pair
+
+
+class MyClass
+  @@cvar = 'Hello, My class variable!'
+
+  def cvar_in_method
+    puts @@cvar
+  end
+
+  def self.cvar_in_method
+    puts @@cvar
+  end
+end
+
+my_object = MyClass.new
+
+my_object.cvar_in_method
+MyClass.cvar_in_method
