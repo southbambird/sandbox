4a51951ccd6ab85e9a42ad0844d1e758dcc2a3b8
Wataru Inoue <southavem@gmail.com>
2018-12-04 08:04:40 +0900

    Update block.rb

--- ruby/perfect_ruby/chap3/block.rb
+++ ruby/perfect_ruby/chap3/block.rb
@@ -56,4 +56,16 @@ people.map &:upcase
 
 
 
+def write_with_lock
+  File.open 'time.txt', 'w' do |f|
+    f.flock File::LOCK_EX
 
+    yield f
+
+    f.flock File::LOCK_UN
+  end
+end
+
+write_with_lock do |f|
+  f.puts Time.now
+end
