25402462155228d5032598d62b5ebb2a232a3a4d
Wataru Inoue <southavem@gmail.com>
2018-12-03 17:17:46 +0900

    Update block.rb

--- ruby/perfect_ruby/chap3/block.rb
+++ ruby/perfect_ruby/chap3/block.rb
@@ -23,3 +23,16 @@ end
 with_current_time do |now|
   puts now.year
 end
+
+
+def block_sample(&block)
+  puts 'stand up'
+
+  block.call if block
+
+  puts 'sit down'
+end
+
+block_sample do
+  puts 'walk'
+end
