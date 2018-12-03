a0e519402a0defba0091b9c214f6fb2867dcb55f
Wataru Inoue <southavem@gmail.com>
2018-12-04 08:01:08 +0900

    Update block.rb

--- ruby/perfect_ruby/chap3/block.rb
+++ ruby/perfect_ruby/chap3/block.rb
@@ -42,3 +42,18 @@ people = %w(Alice Bob Charlie)
 block = Proc.new do |name| puts name end
 people.each &block
                  
+p1 = Proc.new {|val| val.upcase }
+p2 = :upcase.to_proc
+
+p1.call 'hi'
+p2.call 'hi'
+
+people = %w(Alice Bob Carol)
+
+people.map {|person| person.upcase }
+people.map &:upcase
+
+
+
+
+
