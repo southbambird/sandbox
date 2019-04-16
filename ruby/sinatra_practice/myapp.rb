dd84cc531893e99a5e80bef9be0542714ab1312b
Wataru Inoue <southavem@gmail.com>
2019-04-16 13:11:37 +0900

    Update myapp.rb

--- ruby/sinatra_practice/myapp.rb
+++ ruby/sinatra_practice/myapp.rb
@@ -4,3 +4,20 @@ require 'sinatra'
 get '/' do
   'Hello world!'
 end
+
+
+get '/foo' do
+  'Does not match "GET /foo/"'
+end
+
+
+
+get '/hello/:name' do
+  "Hello #{params['name']}!"
+end
+
+get '/hello/:name' do |n|
+  "Hello #{n}"
+end
+
+
