1831b0f501f669870423c93f581d3c8d0a51fd4b
Wataru Inoue <southavem@gmail.com>
2019-04-27 17:46:30 +0900

    Create LogModuleTest.rb

---
+++ ruby/cherry_ruby/chap8/LogModuleTest.rb
@@ -0,0 +1,30 @@
+module Loggable
+  def log(text)
+    puts "[LOG] #{text}"
+  end
+end
+
+class Product
+  include Loggable
+
+  def title
+    log 'title is called.'
+    'A great movie'
+  end
+end
+
+class User
+  include Loggable
+
+  def name
+    log 'name is called.'
+    'Alice'
+  end
+end
+
+
+product = Product.new
+p product.title
+
+user = User.new
+p user.name
