4ae71789f2246472a4c66dcd0ab597d93c931e67
Wataru Inoue <southavem@gmail.com>
2019-05-09 13:51:31 +0900

    Create 14-4.rb

---
+++ ruby/fun_ruby/practice/14-4.rb
@@ -0,0 +1,10 @@
+str = "Ruby is an object oriented programming language"
+count = {}
+str.each_char do |c|
+  count[c] = 0 unless count[c]
+  count[c] += 1
+end
+
+count.keys.sort.each do |c|
+  printf("'%s': %s\n", c, "*" * count[c])
+end
