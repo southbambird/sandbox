fea94cb5887f325b79265776e8adaf129acf1e79
Wataru Inoue <southavem@gmail.com>
2019-05-31 16:54:28 +0900

    コネクションに失敗した旨を、簡易的に表示するよう変更

--- ruby/real_world_http/get.rb
+++ ruby/real_world_http/get.rb
@@ -2,8 +2,15 @@ require 'net/http'
 require 'uri'
 
 url = URI.parse('http://localhost:18888')
-res = Net::HTTP.start(url.host, url.port) do |http|
-  http.get('/')
+
+begin
+  res = Net::HTTP.start(url.host, url.port) do |http|
+    http.get('/')
+  end
+
+  puts res.body
+  
+rescue
+  $stderr.puts "connection failed."
 end
 
-puts res.body
