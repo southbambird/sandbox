f7f0978e7cd4b183dbac8fdab4960f8e5a4062f0
inokano <inokano.hawayu@gmail.com>
2019-10-28 23:11:05 +0900

    Create main.go

---
+++ go/starting_go/chap5/error_prac/main.go
@@ -0,0 +1,23 @@
+package main
+
+import (
+    "fmt"
+)
+
+type MyError struct {
+    Message string
+    ErrCode int
+}
+
+func (e *MyError) Error() string {
+    return e.Message
+}
+
+func RaiseError() error {
+    return &MyError{Message: "エラーが発生しました", ErrCode: 999}
+}
+
+func main() {
+    err := RaiseError()
+    fmt.Println(err.Error)
+}
