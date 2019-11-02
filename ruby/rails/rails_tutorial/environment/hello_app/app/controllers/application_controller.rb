76e918e8b892c85b0d31763d2e0fe44e7d7d5bc6
inokano <inokano.hawayu@gmail.com>
2019-11-02 16:08:53 +0900

    practice 1

--- ruby/rails/rails_tutorial/environment/hello_app/app/controllers/application_controller.rb
+++ ruby/rails/rails_tutorial/environment/hello_app/app/controllers/application_controller.rb
@@ -2,8 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   def hello
-    render html: 'Hello, World!!'
+    render html: 'hola, mundo!'
   end
     
 end
-
