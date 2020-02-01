3bf3b7afb354a13430f4487850656824b60529a4
inochi <inokano.kotori@gmail.com>
2020-02-01 21:54:04 +0900

    try break

--- rust/projects/loops/src/main.rs
+++ rust/projects/loops/src/main.rs
@@ -1,5 +1,11 @@
 fn main() {
+    let mut count = 0;
     loop {
-        println!("again!");
+        println!("again! {}", count);
+        count = count + 1;
+        if count == 10000 {
+            break;
+        }
     }
+    println!("finish!");
 }
