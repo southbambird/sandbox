9b920c6413c0fb9eea22931acb94894bf6320545
inochi <inokano.kotori@gmail.com>
2020-02-01 22:13:56 +0900

    Update main.rs

--- rust/projects/loops/src/main.rs
+++ rust/projects/loops/src/main.rs
@@ -7,5 +7,22 @@ fn main() {
             break;
         }
     }
-    println!("finish!");
+    println!("finish loop!");
+
+    let mut count = 3;
+    while count != 0 {
+        println!("{}!", count);
+
+        count = count - 1;
+    }
+
+    println!("LIFTOFF!!!!!!!");
+
+    let a = [10, 20, 30, 40, 50];
+    let mut index = 0;
+
+    while index < 6 {
+        println!("the value is: {}", a[index]);
+        index = index + 1;
+    }
 }
