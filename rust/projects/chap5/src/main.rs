5b37f458927c4da3c91d6bc764a7c53644a27fc7
inochi <inokano.kotori@gmail.com>
2020-02-05 22:38:07 +0900

    タプルに変更

--- rust/projects/chap5/src/main.rs
+++ rust/projects/chap5/src/main.rs
@@ -1,13 +1,12 @@
 fn main() {
-    let width = 30;
-    let height = 50;
+    let rectangle = (30, 50);
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(width, height)
+        area(rectangle)
     );
 }
 
-fn area(width: u32, height: u32) -> u32 {
-    width * height
+fn area(rectangle: (u32, u32)) -> u32 {
+    rectangle.0 * rectangle.1
 }
