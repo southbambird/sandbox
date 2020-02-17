eba68dc61effbd6f9cee9e2e6ccbd3eee33b3664
inochi <inokano.kotori@gmail.com>
2020-02-17 21:53:12 +0900

    構造体にメソッド実装

--- rust/projects/chap5/src/main.rs
+++ rust/projects/chap5/src/main.rs
@@ -4,6 +4,12 @@ struct Rectangle {
     height: u32,
 }
 
+impl Rectangle {
+    fn area(&self) -> u32 {
+        self.width * self.height
+    }
+}
+
 struct Color(i32, i32, i32);
 struct Point(i32, i32, i32);
 
@@ -15,7 +21,7 @@ fn main() {
 
     println!(
         "The area of the rectangle is {} square pixels.",
-        area(&rectangle)
+        rectangle.area()
     );
 
     println!("rect is {:?}", rectangle);
@@ -23,8 +29,4 @@ fn main() {
     let black = Color(0, 0, 0);
     let mut origin = Point(0, 0, 0);
     // origin = black;
-}
-
-fn area(rectangle: &Rectangle) -> u32 {
-    rectangle.width * rectangle.height
 }
