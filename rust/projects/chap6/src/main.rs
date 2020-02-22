67233d0f122e8f09ff3c7e4b64509dc46588fd58
inochi <inokano.kotori@gmail.com>
2020-02-22 14:46:40 +0900

    Update main.rs

--- rust/projects/chap6/src/main.rs
+++ rust/projects/chap6/src/main.rs
@@ -1,8 +1,14 @@
+#[derive(Debug)]
+enum UsState {
+    Alabama,
+    Alaska,
+}
+
 enum Coin {
     Penny,
     Nickel,
     Dime,
-    Quarter,
+    Quarter(UsState),
 }
 
 fn value_in_cents(coin: Coin) -> u32 {
@@ -10,11 +16,14 @@ fn value_in_cents(coin: Coin) -> u32 {
         Coin::Penny => 1,
         Coin::Nickel => 5,
         Coin::Dime => 10,
-        Coin::Quarter => 25,
+        Coin::Quarter(state) => {
+            println!("State quarter from {:?}!", state);
+            25
+        }
     }
 }
 
 fn main() {
-    let coin: Coin = Coin::Dime;
+    let coin: Coin = Coin::Quarter(UsState::Alaska);
     println!("{}", value_in_cents(coin));
 }
