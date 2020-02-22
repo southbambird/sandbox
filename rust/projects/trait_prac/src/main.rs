d1505a777f73a5ad3d236b1cfc5bc6a4b9b35786
inochi <inokano.kotori@gmail.com>
2020-02-22 15:57:54 +0900

    Update main.rs

--- rust/projects/trait_prac/src/main.rs
+++ rust/projects/trait_prac/src/main.rs
@@ -1,7 +1,27 @@
 fn main() {
-    println!("Hello, world!");
+    let news = NewsArticle {
+        headline: String::from("hoge_headline!"),
+        location: String::from("here"),
+        author: String::from("dareka"),
+        content: String::from("naiyou-dayo!"),
+    };
+
+    println!("news desu! -> {}", news.summarize());
 }
 
 pub trait Summary {
     fn summarize(&self) -> String;
+}
+
+pub struct NewsArticle {
+    pub headline: String,
+    pub location: String,
+    pub author: String,
+    pub content: String,
+}
+
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
 }
