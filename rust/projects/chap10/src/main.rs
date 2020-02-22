02cf04326e9c9a82af9fd674f61eeb8f6d2349a6
inochi <inokano.kotori@gmail.com>
2020-02-22 15:04:49 +0900

    Update main.rs

--- rust/projects/chap10/src/main.rs
+++ rust/projects/chap10/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    let number_list = vec![10, 20, 30, 40, 50];
+
+    let mut largest = number_list[0];
+
+    for number in number_list {
+        if number > largest {
+            largest = number;
+        }
+    }
+
+    println!("The largest number is {}", largest);
+    assert_eq!(largest, 50);
 }
