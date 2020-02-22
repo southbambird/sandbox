6891bf9cd7e3ee545d3c4c8f0a435e15456ef681
inochi <inokano.kotori@gmail.com>
2020-02-22 16:19:29 +0900

    Update main.rs

--- rust/projects/trait_prac/src/main.rs
+++ rust/projects/trait_prac/src/main.rs
@@ -7,12 +7,19 @@ fn main() {
     };
 
     println!("news desu! -> {}", news.summarize());
+
+    let tweet = Tweet {
+        username: String::from("horse_ebooks"),
+        content: String::from("of course, as you probably already know, people"),
+        reply: false,
+        retweet: false,
+    };
+
+    println!("1 new tweet: {}", tweet.summarize());
 }
 
 pub trait Summary {
-    fn summarize(&self) -> String {
-        String::from("(Read more...)")
-    }
+    fn summarize(&self) -> String;
 }
 
 pub struct NewsArticle {
@@ -22,10 +29,21 @@ pub struct NewsArticle {
     pub content: String,
 }
 
-//impl Summary for NewsArticle {
-//    fn summarize(&self) -> String {
-//        format!("{}, by {} ({})", self.headline, self.author, self.location)
-//    }
-//}
+impl Summary for NewsArticle {
+    fn summarize(&self) -> String {
+        format!("{}, by {} ({})", self.headline, self.author, self.location)
+    }
+}
 
-impl Summary for NewsArticle {}
+pub struct Tweet {
+    pub username: String,
+    pub content: String,
+    pub reply: bool,
+    pub retweet: bool,
+}
+
+impl Summary for Tweet {
+    fn summarize(&self) -> String {
+        format!("{}: {}", self.username, self.content)
+    }
+}
