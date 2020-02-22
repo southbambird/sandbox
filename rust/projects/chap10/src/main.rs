16f357290080fcea310d6112007de57fb527b996
inochi <inokano.kotori@gmail.com>
2020-02-22 15:27:53 +0900

    Update main.rs

--- rust/projects/chap10/src/main.rs
+++ rust/projects/chap10/src/main.rs
@@ -1,12 +1,4 @@
-fn main() {
-    let number_list = vec![10, 20, 30, 40, 50];
-
-    let result = largest(&number_list);
-    println!("The largest number is {}", result);
-    assert_eq!(result, 50);
-}
-
-fn largest(list: &[i32]) -> i32 {
+fn largest<T>(list: &[T]) -> T {
     let mut largest = list[0];
 
     for &item in list {
@@ -16,4 +8,16 @@ fn largest(list: &[i32]) -> i32 {
     }
 
     largest
+}
+
+fn main() {
+    let number_list = vec![34, 50, 24, 100, 65];
+
+    let result = largest(&number_list);
+    println!("The largest number is {}", result);
+
+    let char_list = vec!['y', 'm', 'a', 'q'];
+
+    let result = largest(&number_list);
+    println!("The largest char is {}", result);
 }
