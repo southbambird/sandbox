eb1cb45f9d3234fd261745b4f80f076621ad2c69
inochi <inokano.kotori@gmail.com>
2020-03-01 18:20:12 +0900

    Update main.rs

--- rust/projects/minigrep/src/main.rs
+++ rust/projects/minigrep/src/main.rs
@@ -1,21 +1,25 @@
 use std::env;
 use std::fs::File;
-use std::io::prelude::*;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
 
-    let query = &args[1];
-    let filename = &args[2];
+    let config = parse_config(&args);
 
-    println!("Searching for {}", query);
-    println!("In file {}", filename);
+    println!("Searching for {}", config.query);
+    println!("In file {}", config.filename);
 
-    let mut f = File::open(filename).expect("file not found.");
+    let mut f = File::open(config.filename).expect("file not found");
+}
 
-    let mut contents = String::new();
+struct Config {
+    query: String,
+    filename: String,
+}
 
-    f.read_to_string(&mut contents)
-        .expect("something went wrong reading the file.");
-    println!("With text: {}", contents);
+fn parse_config(args: &[String]) -> Config {
+    let query = args[1].clone();
+    let filename = args[2].clone();
+
+    Config { query, filename }
 }
