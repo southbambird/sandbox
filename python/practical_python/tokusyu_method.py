d70df1a3d53e683d71bed65746fb9de1544c6b35
inochi <inokano.kotori@gmail.com>
2020-04-13 22:57:06 +0900

    Update tokusyu_method.py

--- python/practical_python/tokusyu_method.py
+++ python/practical_python/tokusyu_method.py
@@ -32,3 +32,20 @@ print(bool(query))
 
 query = QueryParams({'key': 'value'})
 print(bool(query))
+
+
+
+class Adder:
+    def __init__(self):
+        self._values = []
+    def add(self, x):
+        self._values.append(x)
+    def __call__(self):
+        return sum(self._values)
+
+adder = Adder()
+adder.add(1)
+adder.add(3)
+print(adder())
+adder.add(5)
+print(adder())
