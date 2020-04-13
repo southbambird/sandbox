86a909f7ff325e2d25767e5e1d77a6f6a75f6cbf
inochi <inokano.kotori@gmail.com>
2020-04-13 23:16:13 +0900

    Update tokusyu_method.py

--- python/practical_python/tokusyu_method.py
+++ python/practical_python/tokusyu_method.py
@@ -66,3 +66,10 @@ print(adder())
 
 
 
+class Iterable:
+    def __init__(self, num):
+        self.num = num
+    def __iter__(self):
+        return iter(range(self.num))
+
+print([val for val in Iterable(3)])
