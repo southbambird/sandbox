816b647709c04838c4aee65f78efc8e7290f1aa7
inochi <inokano.kotori@gmail.com>
2020-04-18 23:27:52 +0900

    Update tokusyu_method.py

--- python/practical_python/tokusyu_method.py
+++ python/practical_python/tokusyu_method.py
@@ -86,3 +86,24 @@ class Reverser:
             return self.x.pop()
         except IndexError:
             raise StopIteration()
+
+
+
+from collections import defaultdict
+class CountDict:
+    def __init__(self):
+        self._data = {}
+        self._get_count = defaultdict(int)
+        self._set_count = defaultdict(int)
+    def __getitem__(self, key):
+        self._get_count[key] += 1
+        return self._data[key]
+    def __setitem__(self, key, value):
+        self._set_count[key] += 1
+        self._data[key] = value
+    @property
+    def count(self):
+        return {
+            'set': list(self._set_count.items()),
+            'get': list(self._get_count.items()),
+        }
