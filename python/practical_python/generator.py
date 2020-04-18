5c2826cf41dc6f14bdc4c3c80e75080a650acda1
inochi <inokano.kotori@gmail.com>
2020-04-19 00:03:08 +0900

    Create generator.py

---
+++ python/practical_python/generator.py
@@ -0,0 +1,25 @@
+def inf(n):
+    while True:
+        yield n
+
+#for i in inf(3):
+#    print(i)
+
+def gen_function(n):
+    print('start')
+    while n:
+        print(f'yield: {n}')
+        yield n
+        n -= 1
+
+gen = gen_function(2)
+
+next(gen)
+next(gen)
+#next(gen)
+
+for i in gen_function(2):
+    print(i)
+
+print([i for i in gen_function(5)])
+print(max(gen_function(5)))
