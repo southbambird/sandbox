bcbf42a66dd1ddda824b23367a4170e8083c5bb1
inochi <inokano.kotori@gmail.com>
2020-04-19 16:05:13 +0900

    Update decorater.py

--- python/practical_python/decorater.py
+++ python/practical_python/decorater.py
@@ -2,9 +2,21 @@ from functools import lru_cache
 from time import sleep
 @lru_cache(maxsize=32)
 def heavy_function(n):
-    sleep(3)
+#    sleep(3)
     return n+1
 
 print(heavy_function(2))
 
 print(heavy_function(2))
+
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class Fruit:
+    name: str
+    price: int = 0
+
+apple = Fruit(name='apple', price=128)
+print(apple)
+
+apple.price = 256
