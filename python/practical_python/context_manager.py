86490b55b9ca8aace8a34432d7eeb049cfa7c8a2
inochi <inokano.kotori@gmail.com>
2020-04-20 21:31:09 +0900

    Update context_manager.py

--- python/practical_python/context_manager.py
+++ python/practical_python/context_manager.py
@@ -40,3 +40,21 @@ class Point:
 with Point(x=1, y=2) as p:
     print(p)
     p['z'] = 3
+
+from contextlib import contextmanager
+@contextmanager
+def point(**kwargs):
+    print('__enter__ was called')
+    value = kwargs
+    try:
+        yield value
+    except Exception as e:
+        print(e)
+        raise
+    finally:
+        print('__exit__ was called')
+        print(value)
+
+with point(x=1, y=2) as p:
+    print(p)
+    p['z'] = 3
