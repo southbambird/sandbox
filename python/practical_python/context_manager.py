348dd6f4812a20a8765ed422b3692e7d992e519c
inochi <inokano.kotori@gmail.com>
2020-04-20 21:10:57 +0900

    Update context_manager.py

--- python/practical_python/context_manager.py
+++ python/practical_python/context_manager.py
@@ -2,3 +2,15 @@ with open('some.txt', 'w') as f:
     f.write('python')
 
 print(f.closed)
+
+class ContextManager:
+    def __enter__(self):
+        print('__enter__ was called')
+    def __exit__(self, exc_type, exc_value, traceback):
+        print('__exit__ was called')
+        print(f'{exc_type}')
+        print(f'{exc_value}')
+        print(f'{traceback}')
+
+with ContextManager():
+    print('inside the block')
