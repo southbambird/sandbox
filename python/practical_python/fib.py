d784d1fee5cc0a4bb9a6d8f1a67b10ed2f334fe7
inochi <inokano.kotori@gmail.com>
2020-04-21 22:56:48 +0900

    Update fib.py

--- python/practical_python/fib.py
+++ python/practical_python/fib.py
@@ -1,4 +1,6 @@
 import sys
+import os
+import time
 
 def fibonacci(n):
     a, b = 0, 1
@@ -7,9 +9,23 @@ def fibonacci(n):
     else:
         return a
 
+def elapsed_time(f):
+    def wrapper(*args, **kwargs):
+        st = time.time()
+        v = f(*args, **kwargs)
+        print(f"{f.__name__}: {time.time() - st}")
+        return v
+    return wrapper
+
+@elapsed_time
+def get_sequential(nums):
+    for num in nums:
+        print(fibonacci(num))
+
 def main():
     n = int(sys.argv[1])
-    print(fibonacci(n))
+    nums = [n] * os.cpu_count()
+    get_sequential(nums)
 
 if __name__ == "__main__":
     main()
