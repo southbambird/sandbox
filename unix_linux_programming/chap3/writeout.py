f79446d49f5787bb08ec857cc0e0c56df831eac3
inochi <inokano.kotori@gmail.com>
2020-08-13 11:43:14 +0900

    Update writeout.py

--- unix_linux_programming/chap3/writeout.py
+++ unix_linux_programming/chap3/writeout.py
@@ -4,7 +4,7 @@ BUF_SIZE = 1024
 try:
     output_fd = os.open("output_writeout.txt",os.O_WRONLY | os.O_TRUNC | os.O_CREAT, 0o666)
 
-    while((buf := os.read(sys.stdin.fileno(),BUF_SIZE)) != b'\n'):
+    while((buf := os.read(sys.stdin.fileno(),BUF_SIZE)) != b''):
         os.write(output_fd,buf)
 
     os.close(output_fd)
