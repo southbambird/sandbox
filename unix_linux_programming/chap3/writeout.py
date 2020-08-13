93bc5646970eef46400ffb816e8d65c48b7e3688
inochi <inokano.kotori@gmail.com>
2020-08-13 11:40:21 +0900

    Create writeout.py

---
+++ unix_linux_programming/chap3/writeout.py
@@ -0,0 +1,13 @@
+import os
+import sys
+BUF_SIZE = 1024
+try:
+    output_fd = os.open("output_writeout.txt",os.O_WRONLY | os.O_TRUNC | os.O_CREAT, 0o666)
+
+    while((buf := os.read(sys.stdin.fileno(),BUF_SIZE)) != b'\n'):
+        os.write(output_fd,buf)
+
+    os.close(output_fd)
+
+except OSError as err:
+    print(err)
