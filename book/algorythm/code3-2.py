4d5dc41830621dc0a9792dec4726376f9ccd5124
inochi <k.inochi.mi73@gmail.com>
2021-07-02 18:30:12 +0900

    Create code3-2.py

---
+++ book/algorythm/code3-2.py
@@ -0,0 +1,17 @@
+N = int(input("N:"))
+v = int(input("v:"))
+
+a = list()
+
+for i in range(N):
+    a.append(int(input("a[%d]" % i)))
+
+count_i: int = -1
+
+for i in range(N):
+    if(a[i] == v):
+        count_i = i
+        break
+
+print(count_i)
+
