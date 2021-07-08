3366e3283cba4f239cba6a29ee302fdbb5df6b4a
inochi <k.inochi.mi73@gmail.com>
2021-07-08 23:18:53 +0900

    修正
    参照渡し不可なので、計算結果をreturn するよう変更

--- book/algorythm/chap5/code5-3.py
+++ book/algorythm/chap5/code5-3.py
@@ -1,6 +1,8 @@
-def chmin(a, b):
+def chmin(a: int, b: int) -> int:
     if a > b:
-        a = b
+        return b
+    else:
+        return a
 
 INF = 1 << 60
 
@@ -14,10 +16,9 @@ dp = [INF] * N
 dp[0] = 0
 
 for i in range(1, N):
-    chmin(dp[i], dp[i-1] + abs(h[i] - h[i-1]))
+    dp[i] = chmin(dp[i], dp[i-1] + abs(h[i] - h[i-1]))
     if i > 1:
-        chmin(dp[i], dp[i-2] + abs(h[i] - h[i-2]))
+        dp[i] = chmin(dp[i], dp[i-2] + abs(h[i] - h[i-2]))
 
 
-print(dp)
-print(INF)
+print(dp[N-1])
