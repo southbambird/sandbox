f77245c82adbd9777fda34537fe53ba07beac65a
inochi <k.inochi.mi73@gmail.com>
2021-07-31 23:03:35 +0900

    Create item9.py

---
+++ book/effective_python/chap1/item9.py
@@ -0,0 +1,21 @@
+a = 4
+b = 9
+
+# bad
+for i in range(2, min(a, b) + 1):
+    print('Testing', i)
+    if a % i == 0 and b % i == 0:
+        print('Not coprime')
+        break
+else:
+    print('Coprime')
+
+# good
+def coprime(a, b):
+    for i in range(2, min(a,b) + 1):
+        if a % i == 0 and b % i == 0:
+            return False
+    return True
+
+assert coprime(4, 9)
+assert not coprime(3, 6)
