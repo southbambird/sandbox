21b11b798dec958341cdcdf548d4030ee4761da3
inochi <k.inochi.mi73@gmail.com>
2021-09-09 14:38:08 +0900

    Create item19.py

---
+++ book/effective_python/chap3/item19.py
@@ -0,0 +1,44 @@
+def get_stats(numbers):
+    minimum = min(numbers)
+    maximum = max(numbers)
+    return minimum, maximum
+
+lengths = [63, 73, 72, 60, 67, 66, 71, 61, 72, 70]
+
+minimum, maximum = get_stats(lengths)
+
+print(f'Min: {minimum}, Max: {maximum}')
+
+def get_avg_ratio(numbers):
+    average = sum(numbers) / len(numbers)
+    scaled = [x / average for x in numbers]
+    scaled.sort(reverse=True)
+    return scaled
+
+longest, *middle, shortest = get_avg_ratio(lengths)
+
+print(f'Longest:  {longest:>4.0%}')
+print(f'Shortest: {shortest:>4.0%}')
+
+def get_stats(numbers):
+    minimum = min(numbers)
+    maximum = max(numbers)
+
+    count = len(numbers)
+    average = sum(numbers) / count
+
+    sorted_numbers = sorted(numbers)
+    middle = count // 2
+    if count % 2 == 0:
+        lower = sorted_numbers[middle - 1]
+        upper = sorted_numbers[middle]
+        median = (lower + upper) / 2
+    else:
+        median = sorted_numbers[middle]
+    
+    return minimum, maximum, average, median, count
+
+minimum, maximum, average, median, count = get_stats(lengths)
+
+print(f'Min: {minimum}, Max: {maximum}')
+print(f'Average: {average}, Median: {median}, COunt {count}')
